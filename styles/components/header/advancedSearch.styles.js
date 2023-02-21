f372168620ce035cffc5ec938af124ee2f76ac43
Ashish Gupta <ashish.gupta@bold.com>
2023-02-21 10:53:12 +0530

    Styled components variables

--- styles/components/header/advancedSearch.styles.js
+++ styles/components/header/advancedSearch.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const AdvSearchLink = styled.a`
-    color: ${props => props.theme.colors.primary_light};
+    color: ${props => props.theme.colors.primaryLight};
     text-decoration: none;
     padding-left: 24px;
     &:hover{
