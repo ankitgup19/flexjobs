fa7c94eab12e8e2e7765c586cb64d08439e0a31d
ankit gupta <ankit.gupta@bold.com>
2023-02-27 10:32:11 +0530

    Updated Search

--- src/components/common/Search.jsx
+++ src/components/common/Search.jsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useMediaQuery from "../../helper/useMediaQuery";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { InputSearch, SearchWrap, SearchBtn, Styledform } from "../../../styles/components/common/Search.style";
 
 const Search = (props) => {
+    const isMobile = useMediaQuery("(max-width:1024px)");
     const [jtText, setJtText] = useState("");
     const [location, setLocation] = useState("");
+    const [searchPh, setSearchPh] = useState("Search by job title, keyword, etc.");
+    const [locationPh, setLocationPh] = useState("Search by location");
+
+    useEffect(() => {
+        isMobile && (setSearchPh("Search for Jobs...") || setLocationPh("Location"));
+    });
+
     return (
         <Styledform action="/search" method="get">
             <SearchWrap {...props}>
-                <InputSearch bdrcurve type="text" placeholder={{...props}.v1 ? "Search by job title, keyword, etc." : "Search for Jobs"} onChange={(e) => setJtText(e.target.value)} value={jtText}/>
-                <InputSearch type="text" value={location} placeholder={{...props}.v1 ? "Search by location" : "Location"} onChange={(e)=> setLocation(e.target.value)}/>
+                <InputSearch bdrcurve type="text" placeholder={searchPh} onChange={(e) => setJtText(e.target.value)} value={jtText}/>
+                <InputSearch type="text" value={location} placeholder={locationPh} onChange={(e)=> setLocation(e.target.value)}/>
                 <SearchBtn type="submit"><FontAwesomeIcon icon={faSearch}/></SearchBtn>
             </SearchWrap>
         </Styledform>
