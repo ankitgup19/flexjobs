7acacaea5d7a8e1559a7a16736e2c688512908ee
Ashish Gupta <ashish.gupta@bold.com>
2023-03-07 08:54:36 +0530

    Blog - page - Tabs Description (Card Grid)

--- src/components/blog/BlogTabs.jsx
+++ src/components/blog/BlogTabs.jsx
@@ -1,90 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
-import { BLOG_LATEST } from "../../helper/Blogs.js";
+import { BLOG_ARTICLES } from "../../helper/Blogs.js";
+import { STabs, STabList, STab, STabPanel, CardDeck, Card, CardBody, CardImg, CardImgLink, CardDescription, CardTitle, CardTitleLink } from "../../../styles/components/common/BlogTabs.style.js";
 
-const STabs = styled(Tabs)`
-    padding: 0 1.2rem;
-    margin: 1.1rem 0 0;
-`;
-
-const STabList = styled(TabList)`
-    list-style-type: none;
-    display: flex;
-    border-top: 1px solid #c0c0c0;
-    border-bottom: 1px solid #dee2e6;
-    margin: 0 0 2.4rem;
-    padding: 0;
-`;
 STabList.tabsRole = 'TabList';
-
-const STab = styled(Tab)`
-    margin: 1.6rem 2.4rem -.1rem 0;
-    padding: 1.3rem .4rem;
-    cursor: pointer;
-    color: #6c757d;
-    outline: 0;
-
-    &.is-selected {
-        color: ${(props) => props.theme.colors.primaryLight};
-        border-bottom: 3px solid ${(props) => props.theme.colors.primaryLight};
-    }
-    &:hover,
-    &:focus {
-        text-decoration: underline;
-    }
-`;
 STab.tabsRole = 'Tab';
-
-const STabPanel = styled(TabPanel)`
-    display: none;
-    grid-template-columns: repeat(3, 1fr);
-    grid-column-gap: 4.5rem;
-    grid-row-gap: 2.4rem;
-
-    &.is-selected {
-        display: grid;
-    }
-`;
-const CardDeck = styled.div`
-    border: 1px solid rgba(0,0,0,0.125);
-    border-top-left-radius: 3px;
-    border-top-right-radius: 3px;
-`;
-const Card = styled.div`    
-    height: 100%;
-    @media only screen and (min-width: 992px) {
-        max-width: 400px;
-    }
-`;
-const CardBody = styled.div`
-    padding: 1.6rem;
-`;
-const CardImg = styled.img`
-    width: 100%;
-    height: auto;
-`;
-const CardImgLink = styled.a`
-    display: flex;
-`;
-const CardDescription = styled.p`
-    line-height: ${(props) => props.theme.lineHeight.lg};
-    margin: 0;
-`;
-const CardTitle = styled.h4`
-    margin: 0 0 1.1rem;
-    line-height: 1.2;
-`;
-const CardTitleLink = styled.a`
-    color: ${(props) => props.theme.colors.primaryLight};
-    text-decoration: none;
-    font-size: 1.84rem;
-    font-weight: 500;
-    &:hover {
-        color: ${(props) => props.theme.colors.primaryDark};
-        text-decoration: underline;
-    }
-`;
 STabPanel.tabsRole = 'TabPanel';
 
 const BlogTabs = () => (
@@ -95,13 +14,14 @@ const BlogTabs = () => (
         <STabList>
             <STab>Latest</STab>
             <STab>Trending</STab>
+            <STab>Saved</STab>
         </STabList>
         <STabPanel>
             {
-                BLOG_LATEST.data.map((item, index) =>
+                BLOG_ARTICLES.data.filter((article) => article.type.toLowerCase() === "latest").map((item) =>
                     <CardDeck>
                         <Card>
-                            <CardImgLink href={item.href} className="card-img-link"><CardImg height="174" width="384" alt="" src={item.img_url} /></CardImgLink>
+                            <CardImgLink href={item.href}><CardImg width={item.width} height={item.height} alt={item.alt} src={item.img_url} /></CardImgLink>
                             <CardBody>
                                 <CardTitle>
                                     <CardTitleLink href={item.href}>{item.title}</CardTitleLink>
@@ -112,7 +32,38 @@ const BlogTabs = () => (
                     </CardDeck>)
             }
         </STabPanel>
-        <STabPanel>Panel 2</STabPanel>
+        <STabPanel>
+            {
+                BLOG_ARTICLES.data.filter((article) => article.type.toLowerCase() === "trending").map((item) =>
+                    <CardDeck>
+                        <Card>
+                            <CardImgLink href={item.href}><CardImg width={item.width} height={item.height} alt={item.alt} src={item.img_url} /></CardImgLink>
+                            <CardBody>
+                                <CardTitle>
+                                    <CardTitleLink href={item.href}>{item.title}</CardTitleLink>
+                                </CardTitle>
+                                <CardDescription>{item.description}</CardDescription>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>)
+            }
+        </STabPanel>
+        <STabPanel>
+            {
+                BLOG_ARTICLES.data.filter((article) => article.type.toLowerCase() === "saved").map((item) =>
+                    <CardDeck>
+                        <Card>
+                            <CardImgLink href={item.href}><CardImg width={item.width} height={item.height} alt={item.alt} src={item.img_url} /></CardImgLink>
+                            <CardBody>
+                                <CardTitle>
+                                    <CardTitleLink href={item.href}>{item.title}</CardTitleLink>
+                                </CardTitle>
+                                <CardDescription>{item.description}</CardDescription>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>)
+            }
+        </STabPanel>
     </STabs>
 );
 
--- src/helper/Blogs.js
+++ src/helper/Blogs.js
@@ -1,123 +1,212 @@
-export const BLOG_LATEST = {
-    "data":[{
+export const BLOG_ARTICLES = {
+    "data": [{
         "href": "/blog/post/how-to-lose-a-job-offer-in-no-time",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/08/06104304/How-to-Lose-a-Job-Offer-in-No-Time-353x177.jpg",
-        "title":"6 Mistakes That Can Lose a Job Offer in No Time",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/08/06104304/How-to-Lose-a-Job-Offer-in-No-Time-353x177.jpg",
+        "title": "6 Mistakes That Can Lose a Job Offer in No Time",
         "description": "Certain missteps can cost you a new job offer. Avoid these six mistakes that cause employers to change their mind—plus, how to protect your new opportunity.",
+        "alt": "",
         "width": "384",
         "height": "174",
         "type": "Latest"
     },
     {
         "href": "/blog/post/20-companies-offering-remote-jobs-worldwide",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/02/16051257/20-Companies-Offering-Remote-Jobs-Worldwide-2-353x177.jpg",
-        "title":"20 Companies Offering Remote Jobs Worldwide",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/02/16051257/20-Companies-Offering-Remote-Jobs-Worldwide-2-353x177.jpg",
+        "title": "20 Companies Offering Remote Jobs Worldwide",
         "description": "Want to grow your career and live somewhere new? Explore these companies hiring around the world for remote jobs and find your next opportunity!",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/jobs-for-late-risers",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/01/12073535/10-Flexible-Careers-for-Late-Risers-353x177.jpg",
-        "title":"10 Flexible Jobs for Night Owls and Late Risers",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/01/12073535/10-Flexible-Careers-for-Late-Risers-353x177.jpg",
+        "title": "10 Flexible Jobs for Night Owls and Late Risers",
         "description": "The typical 9-to-5 shift doesn’t work for everyone. If you’re a night owl, explore these 10 careers that are perfect for late risers!",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/flexible-remote-job-supports-man-caring-for-aging-parents",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/16060052/Flexible-Job-Supports-Caring-for-Aging-Parents-353x177.jpg",
-        "title":"Flexible, Remote Job Supports Man Caring for Aging Parents",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/16060052/Flexible-Job-Supports-Caring-for-Aging-Parents-353x177.jpg",
+        "title": "Flexible, Remote Job Supports Man Caring for Aging Parents",
         "description": "Balancing a career and caregiving comes with a unique set of challenges. For Jorge, overcoming those challenges was possible by finding a flexible, remote job.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/ways-deal-rude-hiring-manager",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/01/12082733/6-Ways-to-Deal-With-a-Rude-Hiring-Manager-353x177.jpg",
-        "title":"6 Ways to Deal With a Rude Hiring Manager",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/01/12082733/6-Ways-to-Deal-With-a-Rude-Hiring-Manager-353x177.jpg",
+        "title": "6 Ways to Deal With a Rude Hiring Manager",
         "description": "Has a hiring manager been downright rude? Remaining calm can pay off, whether you take the job or not. Use these strategies to handle rude hiring managers!",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/what-is-a-stem-career-qualifications-salary-and-more",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/13060348/What-Is-a-STEM-Career-353x177.jpg",
-        "title":"What is a STEM Career? Qualifications, Salary, and More",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/13060348/What-Is-a-STEM-Career-353x177.jpg",
+        "title": "What is a STEM Career? Qualifications, Salary, and More",
         "description": "Are you interested in breaking into a STEM field? Learn more about common qualifications, salaries in STEM fields, companies hiring and more.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/fully-remote-companies-hiring-now",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/06/23071114/30-Fully-Remote-Companies-Hiring-Now-353x177.jpg",
-        "title":"30 Fully Remote Companies Hiring Right Now",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/06/23071114/30-Fully-Remote-Companies-Hiring-Now-353x177.jpg",
+        "title": "30 Fully Remote Companies Hiring Right Now",
         "description": "Are you searching for a fully remote job? Explore these 30 fully remote companies hiring right now for remote, work-from-home jobs.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/new-definition-success-controlling-your-time",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/11/02080040/The-New-Definition-of-Success-Controlling-Your-Time-353x177.jpg",
-        "title":"The New Definition of Success: Controlling Your Time",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/11/02080040/The-New-Definition-of-Success-Controlling-Your-Time-353x177.jpg",
+        "title": "The New Definition of Success: Controlling Your Time",
         "description": "Working longer hours doesn’t necessarily mean you’re getting more done. Here’s how to reclaim your time, while accomplishing more during your workday.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/introvert-extrovert-ambivert-means-job-search",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/08/15083706/Introvert-Extrovert-Ambivert-What-Each-Means-for-Your-Job-Search-353x177.jpg",
-        "title":"Introvert, Extrovert, Ambivert: What This Means for Your Job Search",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/08/15083706/Introvert-Extrovert-Ambivert-What-Each-Means-for-Your-Job-Search-353x177.jpg",
+        "title": "Introvert, Extrovert, Ambivert: What This Means for Your Job Search",
         "description": "Many of us aren’t quite an introvert or extrovert, but an ambivert. Here’s how knowing your personality type can help you land your next career opportunity.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/job-search-terms-should-know-complete-glossary",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/02/09062110/Job-Search-Glossary-353x177.jpg",
-        "title":"Job Search Terms Everyone Should Know: A Complete Glossary",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/02/09062110/Job-Search-Glossary-353x177.jpg",
+        "title": "Job Search Terms Everyone Should Know: A Complete Glossary",
         "description": "Have you been stumped by a new word in an application or interview? Here’s a full list of helpful terms and concepts that can help you land your next job.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/what-is-your-ideal-career",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/11/08091244/What-Is-Your-Ideal-Career-353x177.jpg",
-        "title":"What Is Your Ideal Career?",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/11/08091244/What-Is-Your-Ideal-Career-353x177.jpg",
+        "title": "What Is Your Ideal Career?",
         "description": "Finding the best career for you doesn’t have to be difficult! Here’s how to figure out your interests, research careers, and create a long-term plan.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/marketing-resumes-tips-skills-to-include-and-more",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/26094009/Marketing-Resume-Tips-Skills-to-Include-and-More-353x177.jpg",
-        "title":"Marketing Resumes: Tips, Skills to Include and More",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/26094009/Marketing-Resume-Tips-Skills-to-Include-and-More-353x177.jpg",
+        "title": "Marketing Resumes: Tips, Skills to Include and More",
         "description": "Are you applying for marketing jobs? Here’s how to create the best marketing resume, including essential marketing skills, experience, and more.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/hybrid-remote-jobs-companies-that-hire",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/07/26100733/25-Companies-That-Hire-for-Hybrid-Remote-Jobs-2-353x177.jpeg",
-        "title":"25 Companies That Hire for Hybrid Jobs (Partially Remote)",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/07/26100733/25-Companies-That-Hire-for-Hybrid-Remote-Jobs-2-353x177.jpeg",
+        "title": "25 Companies That Hire for Hybrid Jobs (Partially Remote)",
         "description": "Getting back to the office can mean a hybrid workplace, with a mix of in-person and remote work. These companies hire for hybrid remote jobs!",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/modern-versions-common-early-remote-jobs",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/18053912/7-Modern-Versions-of-the-Most-Common-Early-Remote-Jobs-353x177.jpg",
-        "title":"Modern Versions of the Most Common Early Remote Jobs",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/18053912/7-Modern-Versions-of-the-Most-Common-Early-Remote-Jobs-353x177.jpg",
+        "title": "Modern Versions of the Most Common Early Remote Jobs",
         "description": "Remote jobs of today share many characteristics with the earliest professions (also remote jobs!) Seven of these remote careers are also hiring!",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
     },
     {
         "href": "/blog/post/improve-chances-getting-job",
-        "img_url":"https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/12072818/How-to-Improve-Your-Chances-of-Getting-a-Job-353x177.jpg",
-        "title":"How to Improve Your Chances of Getting a Job",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/10/12072818/How-to-Improve-Your-Chances-of-Getting-a-Job-353x177.jpg",
+        "title": "How to Improve Your Chances of Getting a Job",
         "description": "Job searching is competitive, so how can you stand out from other applicants and improve your chances of landing the job? Here are five tips.",
+        "alt": "",
         "width": "384",
-        "height": "174"
+        "height": "174",
+        "type": "Latest"
+    },
+    {
+        "href": "/blog/post/how-to-lose-a-job-offer-in-no-time",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/06/19072848/30-Companies-Switching-to-Long-Term-Remote-Work-2-353x177.jpeg",
+        "title": "6 Mistakes That Can Lose a Job Offer in No Time",
+        "description": "Certain missteps can cost you a new job offer. Avoid these six mistakes that cause employers to change their mind—plus, how to protect your new opportunity.",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
+    },
+    {
+        "href": "/blog/post/20-companies-offering-remote-jobs-worldwide",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/02/15055852/25-Companies-That-Allow-Staff-to-Work-Abroad-353x177.jpg",
+        "title": "20 Companies Offering Remote Jobs Worldwide",
+        "description": "Want to grow your career and live somewhere new? Explore these companies hiring around the world for remote jobs and find your next opportunity!",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
+    },
+    {
+        "href": "/blog/post/jobs-for-late-risers",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2019/08/02070937/skills-for-resume-353x177.png",
+        "title": "10 Flexible Jobs for Night Owls and Late Risers",
+        "description": "The typical 9-to-5 shift doesn’t work for everyone. If you’re a night owl, explore these 10 careers that are perfect for late risers!",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
+    },
+    {
+        "href": "/blog/post/flexible-remote-job-supports-man-caring-for-aging-parents",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/16060052/Flexible-Job-Supports-Caring-for-Aging-Parents-353x177.jpg",
+        "title": "Flexible, Remote Job Supports Man Caring for Aging Parents",
+        "description": "Balancing a career and caregiving comes with a unique set of challenges. For Jorge, overcoming those challenges was possible by finding a flexible, remote job.",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
+    },
+    {
+        "href": "/blog/post/ways-deal-rude-hiring-manager",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2023/01/12082733/6-Ways-to-Deal-With-a-Rude-Hiring-Manager-353x177.jpg",
+        "title": "6 Ways to Deal With a Rude Hiring Manager",
+        "description": "Has a hiring manager been downright rude? Remaining calm can pay off, whether you take the job or not. Use these strategies to handle rude hiring managers!",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
+    },
+    {
+        "href": "/blog/post/what-is-a-stem-career-qualifications-salary-and-more",
+        "img_url": "https://fjwp.s3.amazonaws.com/blog/wp-content/uploads/2022/12/13060348/What-Is-a-STEM-Career-353x177.jpg",
+        "title": "What is a STEM Career? Qualifications, Salary, and More",
+        "description": "Are you interested in breaking into a STEM field? Learn more about common qualifications, salaries in STEM fields, companies hiring and more.",
+        "alt": "",
+        "width": "384",
+        "height": "174",
+        "type": "Trending"
     }]
 }
