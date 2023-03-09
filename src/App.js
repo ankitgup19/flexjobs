0552f7ca8ec3d4e5336d8b15a633d108f2491c5e
ankit gupta <ankit.gupta@bold.com>
2023-03-09 20:15:38 +0530

    conflict resolved

--- src/App.js
+++ src/App.js
@@ -10,20 +10,13 @@ import BlogPost from "./components/blog/BlogPost";
 const App = () => {
     return (
         <Layout>
-<<<<<<< HEAD
             <StyledContainerMd layout>
                 <Blog />
                 <BlogPost />
-=======
-            <StyledContainerMd layout>                
-                <BlogDescription />
-                <Blog />
->>>>>>> 247137cdaa6af6e3e6d6b411db01e8e5f32d0d28
                 <Login />
                 <SignUp />
                 <PasswordRecovery />
                 <SearchResult />
-                {/* <Blog/> */}
             </StyledContainerMd>
         </Layout>
     )
