e6c2204bdf068fc8d54a7254fa14e888f14cfa3b
Ashish Gupta <ashish.gupta@bold.com>
2023-03-20 08:52:53 +0530

    Blog Right Section - Career Coaching | Header content section

--- src/App.js
+++ src/App.js
@@ -10,50 +10,55 @@ import BlogPost from "./components/blog/BlogPost";
 import BlogCategory from "./components/blog/BlogCategory";
 import JobSearch from "./components/careerAdvice/JobSearch";
 import SelectedTopic from "./components/careerAdvice/SelectedTopic.jsx";
+import CareerCoaching from "./components/careerAdvice/CareerCoaching.jsx";
 const App = () => {
     return (
         <Layout>
             <StyledContainerMd layout>
+                {/* <center>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Job Search Career Advice</h1>
+                </center> */}
+                <CareerCoaching />
                 <center>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Job Search Career Advice</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Job Search Career Advice</h1>
                 </center>
                 <JobSearch />
                 <center>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Career Advice Selected Topic </h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Career Advice Selected Topic </h1>
                 </center>
                 <SelectedTopic />
                 <center>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Blog Category</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Blog Category</h1>
                 </center>
                 <BlogCategory />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Blog Post</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Blog Post</h1>
                 </center>
                 <BlogPost />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Blog</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Blog</h1>
                 </center>
                 <Blog />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Login</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Login</h1>
                 </center>
                 <Login />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Signup</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Signup</h1>
                 </center>
                 <SignUp />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Password Recovery</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Password Recovery</h1>
                 </center>
                 <PasswordRecovery />
                 <center>
                     <h1>&nbsp;</h1>
-                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page Search Result</h1>
+                    <h1 style={{ background: '#f1f9fe', padding: '2rem 0' }}>Page - Search Result</h1>
                 </center>
                 <SearchResult />
             </StyledContainerMd>
---
+++ src/components/careerAdvice/CareerCoaching.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FlexLayout } from "../layout/Layout";
+import Breadcrumb from "../common/Breadcrumb";
+import CareerCoachingHeader from "./CareerCoachingHeader.jsx";
+import BlogPromo from "../blog/BlogPromo";
+import BlogSidebarWidgets from "../blog/BlogSidebarWidgets";
+import BlogTestimonialSlider from "../blog/BlogTestimonialSlider";
+import BlogFooterLink from "../blog/BlogFooterLink";
+import BlogNewsletter from "../blog/BlogNewsletter";
+
+const CareerCoaching = () => {
+    return (
+        <FlexLayout filter>
+            <>
+                <p style={{ margin: '0rem 0 0' }}>&nbsp;</p>
+                <Breadcrumb />
+                <CareerCoachingHeader
+                    text='Career Coaching - '
+                    textEmphasized='Temporarily Sold Out'>
+                </CareerCoachingHeader>
+            </>
+            <>
+                <BlogPromo />
+                <BlogSidebarWidgets text="Success Stories Just In!" center>
+                    <BlogTestimonialSlider />
+                    <BlogFooterLink text="Read More Success Stories" extraSpacing />
+                </BlogSidebarWidgets>
+                <BlogSidebarWidgets
+                    text="Weekly Newsletter"
+                    center
+                    iconSrc="https://www.flexjobs.com/images/icons/newletter-graphic.svg"
+                    iconTxt="Weekly Newsletter icon"
+                    iconWidth="64"
+                    headerDirectionColumn
+                >
+                    <BlogNewsletter />
+                </BlogSidebarWidgets>
+            </>
+        </FlexLayout>
+    );
+};
+
+export default CareerCoaching;
