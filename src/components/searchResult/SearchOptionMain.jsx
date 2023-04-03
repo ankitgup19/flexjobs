88cc9cb23e09edb21179708c984a3373633173ea
Ashish Gupta <ashish.gupta@bold.com>
2023-04-03 10:06:50 +0530

    Multi Select Dropdown

--- src/components/searchResult/SearchOptionMain.jsx
+++ src/components/searchResult/SearchOptionMain.jsx
@@ -3,16 +3,41 @@ import styled from 'styled-components';
 import FormGroup from '../common/FormGroup';
 import Popover from '../common/PopoverDefinition';
 import FormCheck from '../common/FormCheck';
+import ReactSelect from 'react-select'
 import { LinkBtn } from '../../../styles/components/common/Common.styles.js';
 
-const REMOTE_LIST = ['Select', 'Remote - Any Level', '100% Remote Work', 'Hybrid Remote Work', 'Option for Remote Work', 'No Remote Work'];
+const REMOTE_LIST = [
+  { value: 'Remote - Any Level', label: 'Remote - Any Level' },
+  { value: '100% Remote Work', label: '100% Remote Work' },
+  { value: 'Hybrid Remote Work', label: 'Hybrid Remote Work' },
+  { value: 'Option for Remote Work', label: 'Option for Remote Work' },
+  { value: 'No Remote Work', label: 'No Remote Work' },
+];
 const INTL_LOC_LIST = ['Select Country', 'Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua and Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'The Bahamas', 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia and Herzegovina', 'Botswana', 'Brazil', 'Brunei', 'Bulgaria', 'Burkina Faso', 'Burma', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Central African Republic', 'Chad', 'Chile', 'China', 'Colombia', 'Comoros', 'Congo (Brazzaville)', 'Congo (Kinshasa)', 'Costa Rica', "Côte d'Ivoire", 'Croatia', 'Cuba', 'Curacao', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Eritrea', 'Estonia', 'Ethiopia', 'Fiji', 'Finland', 'France', 'Gabon', 'The Gambia', 'Georgia', 'Germany', 'Ghana', 'Greece', 'Grenada', 'Guam', 'Guatemala', 'Guinea', 'Guinea-Bissau', 'Guyana', 'Haiti', 'Holy See', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jordan', 'Kazakhstan', 'Kenya', 'Kiribati', 'Kosovo', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Latvia', 'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Marshall Islands', 'Mauritania', 'Mauritius', 'Mexico', 'Federated States of Micronesia', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Morocco', 'Mozambique', 'Namibia', 'Nauru', 'Nepal', 'Netherlands', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'North Korea', 'Norway', 'Oman', 'Pakistan', 'Palau', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Qatar', 'Romania', 'Rwanda', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Martin', 'Saint Vincent and the Grenadines', 'Samoa', 'San Marino', 'Sao Tome and Principe', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Sint Maarten', 'Slovakia', 'Slovenia', 'Solomon Islands', 'Somalia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Timor-Leste', 'Togo', 'Tonga', 'Trinidad and Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Tuvalu', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'Uruguay', 'Uzbekistan', 'Vanuatu', 'Venezuela', 'Vietnam', 'Virgin Islands (British)', 'Virgin Islands (U.S.)', 'Yemen', 'Zambia', 'Zimbabwe'];
-const JOB_TYPES_LIST = ['Employee', 'Freelance', 'Temporary'];
-const WORK_SCHEDULE_LIST = ['Full-Time','Part-Time','Flexible Schedule','Alternative Schedule'];
-const CAREER_LEVEL_LIST = ['Entry-Level','Experienced','Manager','Senior Level Manager'];
-const CATEGORIES_LIST = ['Entry-Level','Experienced','Manager','Senior Level Manager'];
+const JOB_TYPES_LIST = [
+  { value: 'Employee', label: 'Employee' },
+  { value: 'Freelance', label: 'Freelance' },
+  { value: 'Temporary', label: 'Temporary' },
+];
+const WORK_SCHEDULE_LIST = [
+  { value: 'Full-Time', label: 'Full-Time' },
+  { value: 'Part-Time', label: 'Part-Time' },
+  { value: 'Flexible Schedule', label: 'Flexible Schedule' },
+  { value: 'Alternative Schedule', label: 'Alternative Schedule' },
+];
+const CAREER_LEVEL_LIST = [
+  { value: 'Entry-Level', label: 'Entry-Level' },
+  { value: 'Experienced', label: 'Experienced' },
+  { value: 'Manager', label: 'Manager' },
+  { value: 'Senior Level Manager', label: 'Senior Level Manager' },
+];
 const FEATURED_LIST = ['Select', 'Featured Employers', 'FlexJobs Top 100 Remote', 'Diversity-Friendly', 'Environmental/Green', 'Family-Friendly', 'Fast Growing', 'Flex-Friendly Cultures', 'Great Places to Work Awards', 'Healthy Lifestyle Cultures', 'Military-Friendly', 'Other Awards or Accolades', 'Progressive', 'Public Companies', 'Remote-Friendly Cultures', 'Trusted Reputation Awards', 'Women-Friendly', 'All Award-Winning Employers', '100 Best Corporate Citizen', '50 Best Companies for Latinas', '50 Best Nonprofits To Work For', 'America’s 100 Best Adoption-Friendly Workplaces', 'Barrons 400', 'Best Companies to Work for in Florida ', 'Best Companies to Work for in New York', 'Best Companies to Work for in Pennsylvania', 'Best Companies to Work for in Texas', 'Best Employers for Healthy Lifestyles', 'Black Enterprise - 40 Best Companies for Diversity', 'CareerBliss 50 Happiest Companies in America', 'ComputerWorld - Best Places to Work in IT', 'Diversity 50', 'Entrepreneur\'s Best Small and Medium Workplaces', 'Equal Opportunity Top 50 Employers', 'Fast Company - Most Innovative Company', 'Forbes 2000', 'Forbes Most Valuable Brands', 'Forbes - World\'s Most Reputable Companies', 'Forbes World\'s Most Innovative Companies', 'Fortune 100', 'Fortune 500', 'Fortune\'s Most Admired Companies', 'Glassdoor Employees Choice - Best Place to Work', 'Harris Reputation Quotient Survey', 'Human Rights Campaign Best Places to Work', 'Inc 500', 'Minneapolis Star Trib Top Workplaces', 'Most Ethical Companies', 'NYSE100', 'Outside 50', 'Red100', 'S&amp;P 500', 'Top 100 Ideal Employers', 'Training Magazine - Top 125', 'WorldBlu Most Democratic Workplaces'];
-const TRAVEL_LIST = ['Yes, a lot','Yes, a bit','No','No specification'];
+const TRAVEL_LIST = [
+  { value: 'Yes, a lot', label: 'Yes, a lot' },
+  { value: 'Yes, a bit', label: 'Yes, a bit' },
+  { value: 'No', label: 'No' },
+  { value: 'No specification', label: 'No specification' },
+];
 
 const SearchOptionMain = () => {
   const [popover, setPopover] = useState(new Array(11).fill(false));
@@ -72,11 +97,17 @@ const SearchOptionMain = () => {
                 body="If you want to work remotely, choose which option works for you. Recommendation - choose 'Remote Work - Any Level' unless you specifically want '100% Remote Work.' We also suggest entering your location along with your remote work level, so you receive positions that are either available to local candidates in your area, nationally, or globally. To see more about why some remote positions have a location, please <a href='https://www.flexjobs.com/blog/post/why-do-some-telecommuting-jobs-require-a-location/' target='blank'>check out this blog</a>."
               />}
             <div className='controls'>
-              <Select>
-                {REMOTE_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={REMOTE_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
         </div>
@@ -134,11 +165,17 @@ const SearchOptionMain = () => {
                 body='Choose which type of job arrangement that you are interested in or would consider.'
               />}
             <div className='controls'>
-              <Select>
-                {JOB_TYPES_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={JOB_TYPES_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
 
@@ -155,11 +192,17 @@ const SearchOptionMain = () => {
                 body="Choose a schedule that you would consider or prefer. For information on flexible schedules and alternative schedules, please check out our <a href='https://www.flexjobs.com/blog/post/flexible-schedule-vs-alternative-schedule-whats-difference/' target='blank'>blog post</a>."
               />}
             <div className='controls'>
-              <Select>
-                {WORK_SCHEDULE_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={WORK_SCHEDULE_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
         </div>
@@ -178,11 +221,17 @@ const SearchOptionMain = () => {
                 body='Please select your current or desired career level to see the jobs most applicable to your job search.'
               />}
             <div className='controls'>
-              <Select>
-                {CAREER_LEVEL_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={CAREER_LEVEL_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
 
@@ -199,11 +248,17 @@ const SearchOptionMain = () => {
                 body='Choose the career category that you are interested in and/or qualified for. This is a great way to explore job listings in a given career area with a more broad approach than doing a specific keyword search.'
               />}
             <div className='controls'>
-              <Select>
-                {CATEGORIES_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={CAREER_LEVEL_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
         </div>
@@ -243,11 +298,17 @@ const SearchOptionMain = () => {
                 body='Consider whether you are willing to consider a position that has a travel component or not, and select the option that you are comfortable with.'
               />}
             <div className='controls'>
-              <Select>
-                {TRAVEL_LIST.map((item, index) => (
-                  <option key={item + index}>{item}</option>
-                ))}
-              </Select>
+              <ReactSelect
+                options={TRAVEL_LIST}
+                isMulti
+                className="rct-slct-wrp"
+                classNamePrefix="rct-slct"
+                placeholder='Select'
+                isClearable={false}
+                components={{
+                  IndicatorSeparator: () => null
+                }}
+              />
             </div>
           </div>
         </div>
@@ -279,8 +340,31 @@ const SearchOptionMain = () => {
 };
 
 const SearchOptionMainWrap = styled.div`
-  box-shadow: rgba(0,0,0,0.03) 0 2px 0 2px;
-  padding: 1.6rem 0  6rem;
+  box-shadow: rgba(0,0,0,0.03) 0 .2rem 0 .2rem;
+  padding: 1.6rem 0 6rem;
+  .rct-slct-wrp {
+    width: 100%;
+  }
+  .rct-slct__multi-value {
+    background: ${props => props.theme.colors.primaryLight};
+    border-radius: .4rem;
+  }
+  .rct-slct__multi-value__label {
+    color: ${props => props.theme.colors.white}; 
+  }
+  .rct-slct__multi-value__remove {
+    color: ${props => props.theme.colors.white};
+    cursor: pointer;
+    &:hover {
+      background-color: ${props => props.theme.colors.primaryLight};
+      color: ${props => props.theme.colors.white};
+    }
+  }
+  .rct-slct__control:hover,
+  .rct-slct__control--is-focused {
+    box-shadow: none;
+    border-color: ${props => props.theme.colors.tertiartyMedium};
+  }
   .row {
     display: flex;
     flex-wrap: wrap;
